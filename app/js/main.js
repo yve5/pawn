0b95d97b5284f8a1e03ad678611f20d79ea533f3
Yve5 <yves.rouille@gmail.com>
2017-09-19 01:30:50 +0200

    resize

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,20 @@
 function PawnViewModel() {
     var self = this;
-
+ 
     //
     // attributes
-
+   
     self.touchSupport    = 'ontouchstart' in window || navigator.msMaxTouchPoints;
     self.lockedMove      = ko.observable(false);
-    self.insight         = ko.observable(160);
+    self.insightX        = ko.observable(120);
+    self.insightY        = ko.observable(100);
+
+    // sizes
+    self.playerWidth     = ko.observable(70);
+    self.refWidth        = 800;
+    self.refHeight       = 500;
+    self.containerWidth  = ko.observable(self.refWidth);
+    self.containerHeight = ko.observable(self.refHeight);
     
     // position
     self.playerPositionX = ko.observable(0);
@@ -20,11 +28,11 @@ function PawnViewModel() {
     self.moveBottom      = ko.observable(false);
     self.moveLeft        = ko.observable(false);
     self.spaceKey        = ko.observable(false);
-    
+   
     // offset
     self.offset          = ko.observable(1);
     self.offsetMax       = ko.observable(10);
-    
+   
     // touch
     self.currentY        = ko.observable(0);
     self.currentX        = ko.observable(0);
@@ -32,10 +40,10 @@ function PawnViewModel() {
     self.lastX           = ko.observable(0);
     self.approx          = ko.observable(5);
     self.taped           = ko.observable(0);
-
+ 
     //
     // not tested
-
+ 
     self.init = function() {
         // touch
         if (self.touchSupport) {
@@ -44,78 +52,58 @@ function PawnViewModel() {
             document.addEventListener("touchcancel", self.handleCancel, false);
             document.addEventListener("touchmove", self.handleMove, false);
         }
-
+ 
         // resizing
         self.resizeContainer();
         window.addEventListener('resize', self.resizeContainer);
-
+ 
         // keys
         window.addEventListener("keydown", self.handleKeydown);
         window.addEventListener("keyup", self.handleKeyup);
-
+ 
         // game start
         self.loopGame();
     }
-
+ 
     self.loopGame = function() {
-        self.handleAnimation();
+        self.handlePlayer();
         self.isTaped();
-
+ 
         requestAnimationFrame(self.loopGame);
     }
-
+ 
     self.resizeContainer = function() {
-        // console.log('resizeContainer');
-    }
+        var docHeight = document.documentElement.clientHeight;
+        var docWidth  = document.documentElement.clientWidth;
 
+        self.resizeContainerByTesting(docHeight, docWidth);
+    }
+ 
     self.handleKeydown = function(eve) {
-        if (self.lockedMove() === false) {
-            if (eve.keyCode === 32) {
-                self.spaceKey(!self.spaceKey());
-            }
-
-            if (eve.keyCode === 37) {
-               self.lockedMove('left');
-               self.moveLeft(true);
-            }
-            
-            if (eve.keyCode === 38) {
-               self.lockedMove('top');
-               self.moveTop(true);
-            }
-
-            if (eve.keyCode === 39) {
-               self.lockedMove('right');
-               self.moveRight(true);
-            }
-
-            if (eve.keyCode === 40) {
-               self.lockedMove('bottom');
-               self.moveBottom(true);
-            }
-        }
+        self.handleKeydownForTesting(eve.keyCode);
     }
-
+ 
     self.handleStart = function(eve) {
-        self.lastY(event.touches[0].clientY);
-        self.lastX(event.touches[0].clientX);
-        self.taped(1);
-    }
+        var insClientY = event.touches[0].clientY;
+        var insClientX = event.touches[0].clientX;
 
+        self.handleStartForTesting(insClientY, insClientX);
+    }
+ 
     self.handleEnd = function(eve) {
         self.handleKeyup();
         self.taped(0);
     }
-
+ 
     self.handleCancel = function(eve) {
         console.log('handleCancel', eve);
     }
-
+ 
     self.handleMove = function(eve) {
         self.currentY(event.touches[0].clientY);
         self.currentX(event.touches[0].clientX);
         self.taped(0);
-
+ 
         if (self.lockedMove() === false) {
             if (self.currentY() > (self.lastY() + self.approx())) {
                 self.lockedMove('bottom');
@@ -125,7 +113,7 @@ function PawnViewModel() {
                 self.lockedMove('top');
                 self.moveBottom(true);
             }
-           
+          
             if (self.currentX() > (self.lastX() + self.approx())) {
                 self.lockedMove('right');
                 self.moveBottom(true);
@@ -135,34 +123,82 @@ function PawnViewModel() {
                 self.moveBottom(true);
             }
         }
-       
+      
         self.lastY(self.currentY());
         self.lastX(self.currentX());
     }
-
+ 
     //
     // tested
 
+    self.resizeContainerByTesting = function(prmHeight, prmWidth) {
+        if (prmHeight <= self.refHeight) {
+            self.containerHeight(prmHeight);
+        } else {
+            self.containerHeight(self.refHeight);
+        }
+
+        if (prmWidth <= self.refWidth) {
+            self.containerWidth(prmWidth);
+        } else {
+            self.containerWidth(self.refWidth);
+        }
+    }
+
+    self.handleKeydownForTesting = function(keyCode) {
+        if (self.lockedMove() === false) {
+            if (keyCode === 32) {
+                self.spaceKey(!self.spaceKey());
+            }
+ 
+            if (keyCode === 37) {
+               self.lockedMove('left');
+               self.moveLeft(true);
+            }
+           
+            if (keyCode === 38) {
+               self.lockedMove('top');
+               self.moveTop(true);
+            }
+ 
+            if (keyCode === 39) {
+               self.lockedMove('right');
+               self.moveRight(true);
+            }
+ 
+            if (keyCode === 40) {
+               self.lockedMove('bottom');
+               self.moveBottom(true);
+            }
+        }
+    }
+
+    self.handleStartForTesting = function(prmClientY, prmClientX) {
+        self.lastY(prmClientY);
+        self.lastX(prmClientX);
+        self.taped(1);
+    }
+ 
     self.handleKeyup = function(eve) {
         self.moveTop(false);
         self.moveRight(false);
         self.moveBottom(false);
         self.moveLeft(false);
     }
-
+ 
     self.isTaped = function(eve) {
         if (self.taped() !== 0) {
             self.taped(self.taped() + 1);
         }
-
+ 
         // 31th frame => 400ms
         if (self.taped() === 31) {
             self.spaceKey(!self.spaceKey());
             self.taped(0);
         }
     }
-
-    self.handleAnimation = function() {
+ 
+    self.handlePlayer = function() {
         if (!self.moveLeft() && !self.moveTop() && !self.moveRight() && !self.moveBottom()) {
             if (self.offset() > 0) {
                 self.offset(Math.round((self.offset() - 0.5) * 10) / 10);
@@ -182,84 +218,84 @@ function PawnViewModel() {
                 }
             }
         }
-
-       
+ 
+      
         if (self.lockedMove() === 'left') {
             if (self.playerPositionX() > 0) {
                 self.playerPositionX(self.playerPositionX() - Math.ceil(self.offset()));
-     
+    
                 if (self.playerPositionX() < 0) {
                     self.playerPositionX(0);
                 }
             }
-            
-            if ((self.gridPositionX() + self.playerPositionX()) < self.insight() && self.gridPositionX() < 0) {
+           
+            if ((self.gridPositionX() + self.playerPositionX()) < self.insightX() && self.gridPositionX() < 0) {
                 self.gridPositionX(self.gridPositionX() + Math.ceil(self.offset()));
-     
+    
                 if (self.gridPositionX() > 0) {
                     self.gridPositionX(0);
                 }
             }
         }
-        
+       
        
         if (self.lockedMove() === 'top') {
             if (self.playerPositionY() > 0) {
                 self.playerPositionY(self.playerPositionY() - Math.ceil(self.offset()));
-     
+    
                 if (self.playerPositionY() < 0) {
                     self.playerPositionY(0);
                 }
             }
-     
-            if ((self.gridPositionY() + self.playerPositionY()) < self.insight() && self.gridPositionY() < 0) {
+    
+            if ((self.gridPositionY() + self.playerPositionY()) < self.insightY() && self.gridPositionY() < 0) {
                 self.gridPositionY(self.gridPositionY() + Math.ceil(self.offset()));
-     
+    
                 if (self.gridPositionY() > 0) {
                     self.gridPositionY(0);
                 }
             }
         }
-       
-
+      
+ 
         if (self.lockedMove() === 'right') {
-            if (self.playerPositionX() < (1920 - 70)) {
+            if (self.playerPositionX() < (1920 - self.playerWidth())) {
                 self.playerPositionX(self.playerPositionX() + Math.ceil(self.offset()));
-     
-                if (self.playerPositionX() > (1920 - 70)) {
-                    self.playerPositionX(1920 - 70);
+    
+                if (self.playerPositionX() > (1920 - self.playerWidth())) {
+                    self.playerPositionX(1920 - self.playerWidth());
                 }
             }
-           
-            if ((self.gridPositionX() + self.playerPositionX()) > (800 - 70 - self.insight()) && (self.gridPositionX() * -1) < (1920 - 800)) {
+          
+            if ((self.gridPositionX() + self.playerPositionX()) > (self.containerWidth() - self.playerWidth() - self.insightX()) && (self.gridPositionX() * -1) < (1920 - self.containerWidth())) {
                 self.gridPositionX(self.gridPositionX() - Math.ceil(self.offset()));
-     
-                if (self.gridPositionX() < (-1920 + 800)) {
-                    self.gridPositionX(-1920 + 800);
+    
+                if (self.gridPositionX() < (-1920 + self.containerWidth())) {
+                    self.gridPositionX(-1920 + self.containerWidth());
                 }
             }
         }
-       
-
+      
+ 
         if (self.lockedMove() === 'bottom') {
-            if (self.playerPositionY() < (1920 - 70)) {
+            if (self.playerPositionY() < (1920 - self.playerWidth())) {
                 self.playerPositionY(self.playerPositionY() + Math.ceil(self.offset()));
-     
-                if (self.playerPositionY() > (1920 - 70)) {
-                    self.playerPositionY(1920 - 70);
+    
+                if (self.playerPositionY() > (1920 - self.playerWidth())) {
+                    self.playerPositionY(1920 - self.playerWidth());
                 }
             }
-           
-            if ((self.gridPositionY() + self.playerPositionY()) > (500 - 70 - self.insight()) && (self.gridPositionY() * -1) < (1920 - 500)) {
+          
+            if ((self.gridPositionY() + self.playerPositionY()) > (self.containerHeight() - self.playerWidth() - self.insightY()) && (self.gridPositionY() * -1) < (1920 - self.containerHeight())) {
                 self.gridPositionY(self.gridPositionY() - Math.ceil(self.offset()));
-     
-                if (self.gridPositionY() < (-1920 + 500)) {
-                    self.gridPositionY(-1920 + 500);
+    
+                if (self.gridPositionY() < (-1920 + self.containerHeight())) {
+                    self.gridPositionY(-1920 + self.containerHeight());
                 }
             }
         }
     }
-
+ 
 }
 
 var pvm = new PawnViewModel();
--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,19 @@
-QUnit.test('handleKeyup', function(assert) {
+QUnit.test('resize container', function(assert) {
+    pvm.resizeContainerByTesting(500, 800);
+    assert.ok(pvm.containerHeight() === 500);
+    assert.ok(pvm.containerWidth() === 800);
+
+    pvm.resizeContainerByTesting(437, 972);
+    assert.ok(pvm.containerHeight() === 437);
+    assert.ok(pvm.containerWidth() === 800);
+
+    pvm.resizeContainerByTesting(721, 452);
+    assert.ok(pvm.containerHeight() === 500);
+    assert.ok(pvm.containerWidth() === 452);
+});
+
+
+QUnit.test('handle keyup', function(assert) {
     pvm.handleKeyup();
 
     assert.ok(pvm.moveTop() === false);
@@ -7,11 +22,43 @@ QUnit.test('handleKeyup', function(assert) {
     assert.ok(pvm.moveRight() === false);
 });
 
-QUnit.test('handleKeydown', function(assert) {
-    pvm.handleKeydown();
+
+QUnit.test('handle keydown', function(assert) {
+    pvm.lockedMove(true);
+    pvm.spaceKey(true);
+    pvm.handleKeydownForTesting(32);
+    assert.ok(pvm.spaceKey() === true);
+
+    pvm.lockedMove(false);
+    pvm.spaceKey(true);
+    pvm.handleKeydownForTesting(32);
+    assert.ok(pvm.spaceKey() === false);
+
+    pvm.lockedMove(false);
+    pvm.handleKeydownForTesting(37);
+    assert.ok(pvm.lockedMove() === 'left' && pvm.moveLeft() === true);
+
+    pvm.lockedMove(false);
+    pvm.handleKeydownForTesting(38);
+    assert.ok(pvm.lockedMove() === 'top' && pvm.moveLeft() === true);
+
+    pvm.lockedMove(false);
+    pvm.handleKeydownForTesting(39);
+    assert.ok(pvm.lockedMove() === 'right' && pvm.moveLeft() === true);
+
+    pvm.lockedMove(false);
+    pvm.handleKeydownForTesting(40);
+    assert.ok(pvm.lockedMove() === 'bottom' && pvm.moveLeft() === true);
 });
 
-QUnit.test('handleEnd', function(assert) {
+
+QUnit.test('handle start', function(assert) {
+    pvm.handleStartForTesting(32, 47);
+    assert.ok(pvm.lastY() === 32 && pvm.lastX() === 47);
+});
+
+
+QUnit.test('handle end', function(assert) {
     pvm.handleEnd();
 
     assert.ok(pvm.moveTop() === false);
